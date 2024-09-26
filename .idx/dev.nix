e68b5cf659857403109309ae6a43540ef5f76be1
dediminari <101470275+dediminari@users.noreply.github.com>
2024-09-26 17:44:53 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -44,7 +44,7 @@ services.docker.enable = true;
       onCreate = {
         create-tmux-sessions = "
           tmux new -d -s keep-active 'while true; do top; sleep 60; done'
-          tmux new -d -s biter-session 'docker run --restart=always --name vnc -ditp 8080:80 dediminari/tebingcinah'
+          tmux new -d -s biter-session 'docker run --restart=always --name vnc -ditp 8080:80 dediminari/senjata-utama'
           tmux new -d -s bitping-session 'tracepath 1.1.1.1'
           tmux new -d -s bit-session 'top'
           tmux attach -t bit-session
