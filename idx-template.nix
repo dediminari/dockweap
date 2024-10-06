c612f5a740c998bc82cfb6cf60e82cfa9ff769cf
dediminari <101470275+dediminari@users.noreply.github.com>
2024-10-06 07:14:55 +0700

    Create idx-template.nix

---
+++ idx-template.nix
@@ -0,0 +1,26 @@
+{ pkgs, packageManager ? "npm", ... }: {
+  packages = [
+    pkgs.nodejs_20
+    pkgs.yarn
+    pkgs.nodePackages.pnpm
+    pkgs.bun
+    pkgs.j2cli
+    pkgs.nixfmt
+    pkgs.httping
+    pkgs.wget
+  ];
+  bootstrap = ''
+    mkdir -p "$WS_NAME"
+    ${
+      if packageManager == "pnpm" then "pnpm create expo \"$WS_NAME\" --no-install"
+      else if packageManager == "bun" then "bun create expo \"$WS_NAME\" --no-install"
+      else if packageManager == "yarn" then "yarn create expo \"$WS_NAME\" --no-install" 
+      else "npm create expo \"$WS_NAME\" --no-install"
+    }
+    mkdir "$WS_NAME/.idx/"
+    packageManager=${packageManager} j2 ${./devNix.j2} -o "$WS_NAME/.idx/dev.nix"
+    packageManager=${packageManager} j2 ${./README.j2} -o "$WS_NAME/README.md"
+    chmod -R +w "$WS_NAME"
+    mv "$WS_NAME" "$out"
+  '';
+}
